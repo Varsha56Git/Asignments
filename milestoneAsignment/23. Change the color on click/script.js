b73b108c9695f7387c7c95b0d4b2ef9dc8e092ff
Varsha <128477235+Varsha56Git@users.noreply.github.com>
2024-02-02 10:21:10 +0530

    Create script.js

---
+++ milestoneAsignment/23. Change the color on click/script.js
@@ -0,0 +1,38 @@
+const colors = [
+    "red",
+    "blue",
+    "yellow",
+    "grey",
+    "pink",
+    "cyan",
+    "green",
+    "orange",
+    "darkblue",
+     "brown"
+];
+
+const button = document.getElementById("btn")
+button.addEventListener("click", colorChanger);
+
+function colorChanger(){
+    let value = Math.floor(Math.random()*10)
+    document.body.style.background = colors[value]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
